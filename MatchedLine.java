91ec17f21de75b74b5a178e2be47eaf8e2d3a12f
Danny-Scholar <tochidaniel431@gmail.com>
2025-12-07 04:31:56 -0500

    Delete MatchedLine.java

--- MatchedLine.java
+++
@@ -1,9 +0,0 @@
-public class MatchedLine {
-    public int oldLineNumber;
-    public int newLineNumber;
-
-    public MatchedLine(int oldLineNumber, int newLineNumber) {
-        this.oldLineNumber = oldLineNumber;
-        this.newLineNumber = newLineNumber;
-    }
-}
